6d7363204e3e57c4ce09a19a4614d563e3f8d5fa
Schlegel, Dr. Fabian <27417845+fs82@users.noreply.github.com>
2018-02-25 17:09:01 +0100

    Fixed path error and spelling errors

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-quelle=/mnt/disk1/share/
-ziel=/media/usbdisc/
+quelle=/mnt/disk1/
+ziel=/media/usbdisk/
 heute=$(date +%Y-%m-%d)
 
 if [ `id -u` -eq 0 ]; then
@@ -11,23 +11,23 @@ if [ `id -u` -eq 0 ]; then
 
   #Check if mounting was successfull
   if [ $? -eq 0 ]; then
-  
-    echo "Successfully mounted external disc for backup."
-  
+
+    echo "Successfully mounted external disk for backup."
+
     #Copy backup to external disc
     rsync -avR --delete "${quelle}"  "${ziel}${heute}/" --link-dest="${ziel}last/"
     ln -nsf "${ziel}${heute}" "${ziel}last"
-  
+
     #Unmount external disc
     umount $ziel
 
     if [ $? -eq 0 ]; then
-      echo "External disc was unmouted successfully."
+      echo "External disk was unmouted successfully."
     else
-      echo "Problems occured while unmounting of external disc."
+      echo "Problems occured while unmounting of external disk."
     fi
   else
-    echo "Not possible to mount external disc for backup"
+    echo "Not possible to mount external disk for backup"
   fi 
 else
   echo "Please run as root"
